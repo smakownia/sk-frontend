97f83357d7a756db367033fc7706af99ab5db7f0
aruzo <dawid12391111@gmail.com>
2023-08-11 19:01:59 +0200

    add user menu in navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import { Menu } from "@headlessui/react";
-import { Logo, UserIcon } from "@/components";
+import { Logo } from "@/components";
 import { BasketButton } from "./basket-button";
 import { NavMenuDesktop, NavMenuMobile, NavMenuMobileButton } from "./nav-menu";
+import { UserMenu } from "./user-menu";
 
 const menu = [
   { name: "Strona Główna", href: "/" },
@@ -12,7 +13,10 @@ const menu = [
 
 export function Navbar() {
   return (
-    <nav className="z-10 fixed top-0 left-0 flex items-center h-16 w-full border-b border-neutral-50 bg-white">
+    <nav
+      className={`z-10 fixed top-0 left-0 flex items-center h-16 w-full 
+                  border-b border-neutral-50 bg-white`}
+    >
       <div className="container flex items-center h-full">
         <Link href="/" aria-label="Strona główna">
           <Logo />
@@ -26,13 +30,7 @@ export function Navbar() {
               <BasketButton />
             </li>
             <li>
-              <Link
-                className="btn btn-primary-light px-2"
-                href="/admin"
-                aria-label="Panel admina"
-              >
-                <UserIcon />
-              </Link>
+              <UserMenu />
             </li>
             <li className="md:hidden">
               <NavMenuMobileButton />
