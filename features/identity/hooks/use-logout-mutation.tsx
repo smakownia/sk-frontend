f2d6c254e8e6409ec00ee3d5858352a12e58d324
Dawid Ostrzyżek <64381826+aruzo1@users.noreply.github.com>
2023-08-16 14:33:10 +0200

    Feature/19 refetch basket on logout register and login (#20)
    * add refetch basket on logout, register and login
    
    * add withCredentials to logout

--- features/identity/hooks/use-logout-mutation.tsx
+++ features/identity/hooks/use-logout-mutation.tsx
@@ -1,10 +1,14 @@
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import { logout, useIdentity } from "@/features/identity";
 
 export function useLogoutMutation() {
+  const queryClient = useQueryClient();
   const identity = useIdentity();
 
   return useMutation(logout, {
-    onSuccess: () => identity.logout(),
+    onSuccess: () => {
+      identity.logout();
+      queryClient.invalidateQueries("basket");
+    },
   });
 }
