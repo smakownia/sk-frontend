a89c522b276925eaa9a978d40de9d1de068c6802
aruzo <dawid12391111@gmail.com>
2023-08-16 10:12:16 +0200

    add identity protected

--- pages/admin/kategorie/index.tsx
+++ pages/admin/kategorie/index.tsx
@@ -1,23 +1,26 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { IdentityProtected, IdentityRole } from "@/features/identity";
 import { CategoriesTable } from "@/features/products";
 
 const AdminCategoriesPage: NextPage = () => {
   return (
-    <div className="container mt-10 py-16">
-      <Head>
-        <title>Kategorie - Smakownia</title>
-      </Head>
+    <IdentityProtected role={IdentityRole.Admin}>
+      <div className="container mt-10 py-16">
+        <Head>
+          <title>Kategorie - Smakownia</title>
+        </Head>
 
-      <h1 className="mb-8 text-4xl font-medium">Kategorie</h1>
+        <h1 className="mb-8 text-4xl font-medium">Kategorie</h1>
 
-      <Link className="btn mb-8" href="/admin/kategorie/stworz">
-        Stwórz kategorie
-      </Link>
+        <Link className="btn mb-8" href="/admin/kategorie/stworz">
+          Stwórz kategorie
+        </Link>
 
-      <CategoriesTable />
-    </div>
+        <CategoriesTable />
+      </div>
+    </IdentityProtected>
   );
 };
 
